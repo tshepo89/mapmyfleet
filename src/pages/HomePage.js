313575d1e7afe6e3de87c015f2d04b624f5eea36
xolani_masuku <mcxolani@gmail.com>
2017-04-03 15:54:01 +0200

    add pages

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+    render() {
+        return (
+            <div>
+                HomeApp
+            </div>
+        );
+    }
+}
+
+export default HomePage;
